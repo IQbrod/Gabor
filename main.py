89f3d1dfbf122bc2c0ec29d60ecbebd8e90076af
IQbrod <iqbrod@outlook.fr>
2019-01-23 10:22:52 +0100

    Partie 1

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import lib
 import math
 
-gk = lib.wave_kernel(lambd=24, theta = math.pi/3)
-lib.kernel_plot(gk)
+# -- PARTIE 1 -- Jouer avec les filtres -- #
+
+# gk = lib.wave_kernel(lambd=24, theta = math.pi/3)
+# lib.kernel_plot(gk)
+
+# gk = lib.gaussian_kernel(lambd=24, theta = math.pi/3)
+# lib.kernel_plot(gk)
+
+# gk = lib.gabor_kernel(lambd=12, theta = math.pi/3, n = 256)
+# gk = lib.gabor_kernel(lambd=12, theta = math.pi/3, sl = 1.0, st = 1.0)
+# gk = lib.gabor_kernel(lambd=12, theta = math.pi/3, nl = 2.5)
+# lib.kernel_plot(gk)
