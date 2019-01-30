d8c277843f95aba055c0ef85a8040145610125f6
IQbrod <iqbrod@outlook.fr>
2019-01-30 10:50:44 +0100

    Fin TP

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import lib
 import math
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+from scipy import signal
 
 # -- PARTIE 1 -- Jouer avec les filtres -- #
 
@@ -13,3 +17,11 @@ import math
 # gk = lib.gabor_kernel(lambd=12, theta = math.pi/3, sl = 1.0, st = 1.0)
 # gk = lib.gabor_kernel(lambd=12, theta = math.pi/3, nl = 2.5)
 # lib.kernel_plot(gk)
+
+img = mpimg.imread('src/img2.jpg')
+
+gk = lib.gabor_kernel(lambd=24, theta = math.pi/3)
+gab = signal.convolve2d(img, gk, boundary='symm', mode='same')
+plt.imshow(np.absolute(gab), cmap="gray")
+
+plt.show()
